6d9396cbc0b85604e23f943c0d7a2967e27bb09c
halee <halee@halees-mbp.home>
2018-01-29 09:07:43 -0600

    clean up code and add comments

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -40,8 +40,7 @@ main_page_content = '''
 </html>
 '''
 
-
-
+# each card contains a front with poster & title and flips to back to show storyline & button for movie trailer modal
 movie_cards = '''
         <div class="col-sm">
             <div class="card-container">
@@ -76,10 +75,6 @@ def create_movie_card_content(movies):
     content = ''
     counter = 0
     for movie in movies:
-        # Extract the youtube ID from the url
-        youtube_id_match = re.search(r'(?<=v=)[^&#]+', movie.youtube_trailer)
-        youtube_id_match = youtube_id_match or re.search(r'(?<=be/)[^&#]+', movie.youtube_trailer)
-        trailer_youtube_id = youtube_id_match.group(0) if youtube_id_match else None
 
         # Append the tile for the movie with its content filled in
         content += movie_cards.format(
--- script.js
+++ script.js
@@ -5,8 +5,13 @@
 
         for (var i = 0; i < triggers.length; i++) {
 
+
             $('#myModal' + i).on('hidden.bs.modal', function (index) {
-                return function (e) {
+                return function () {
+                    /* when modal is hidden, set original src link to blank and
+                    then set it back to the duplicated src link so next click on button
+                    returns trailer instead of blank modal
+                     */
                     var dupSrc = $('#myFrame' + index).attr('src');
                     $('#myFrame' + index).attr('src', '');
                     $('#myFrame' + index).attr('src', dupSrc);
