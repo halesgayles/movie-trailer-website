b14ae41512d26fddb798f05407e850b0891e0493
halee <halee@halees-mbp.home>
2018-01-29 11:05:46 -0600

    add comments to class functions in media.py

--- media.py
+++ media.py
@@ -3,10 +3,22 @@ import webbrowser
 
 class Movie():
     def __init__(self, movie_title, storyline, movie_poster, movie_trailer):
+        """
+        initializes the info for each card with all of the info
+
+        :param movie_title: title of movie
+        :param storyline: storyline of movie
+        :param movie_poster: poster for movie
+        :param movie_trailer: youtube trailer for movie
+        """
         self.title = movie_title
         self.storyline = storyline
         self.poster_img = movie_poster
         self.youtube_trailer = movie_trailer
 
     def show_trailer(self):
+        """
+        uses webbrowser to open youtube_trailer and display in browser
+        :return: nothing
+        """
         webbrowser.open(self.youtube_trailer)
